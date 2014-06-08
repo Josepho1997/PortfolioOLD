26cc16b394414dfe535fef5d0f31360b81873130
Joseph <josephoneill1997@gmail.com>
2014-06-07 22:08:37 -0400

    Sticky Footer!

---
+++ js/plasma/plasma.js
@@ -0,0 +1,16 @@
+/*** Use jQuery to support all screen resolutions ***/
+
+$(document).ready(function(){
+	var contentHeight = $("#content").height();
+	var screenHeight =  screen.height;
+	var difference = (screenHeight-contentHeight)/16;
+	
+	$("#push").css("height", difference + 'em');
+	
+	$(window).resize(function(){
+		 contentHeight = $("#content").height();
+		 windowHeight =  $(window).height();
+		 difference = (screenHeight-contentHeight)/16;
+		$("#push").css("height", difference + 'em');
+	 });
+});
