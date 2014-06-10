800909e1ba7b8ad0d5b2568eb1aa7b475e4fd752
Joseph <josephoneill1997@gmail.com>
2014-06-09 22:46:11 -0400

    Set a timeout

--- js/plasma/plasma.js
+++ js/plasma/plasma.js
@@ -2,16 +2,16 @@
 
 $(document).ready(function(){
 	/** Set variables **/
+	setTimeout(function() {
 	var contentHeight = $("#content").height();
 	var footerHeight = $("#footer").height();
 	var screenHeight =  screen.height;
 	var difference = (screenHeight-contentHeight-footerHeight)/16;
+	$("#push").css("height", difference + 'em');
+	}, 300);
 	
 	/** Run slideshow **/
 	runSlideShow(20, 300);
-		
-	$("#push").css("height", difference + 'em');
-
 	
 	var imgHeight = $("#imgreference").height()/16;
 	$("#plasmacontent").css("margin-top", imgHeight + 'em');
